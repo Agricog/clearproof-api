b8ecb1591fc145ce7bd6aa750ca8784f9cda351d
Agricog <mick@mickstevenson.com>
2025-12-18 19:32:27 +0000

    Update smartsuite.ts

--- src/services/smartsuite.ts
+++ src/services/smartsuite.ts
@@ -3,10 +3,10 @@ const WORKSPACE_ID = 'sba974gi'
 const BASE_URL = 'https://app.smartsuite.com/api/v1/applications'
 
 const TABLES = {
-  modules: '69441e0f081da2e01f4d9a79',
-  workers: '69441f0eeb5683351ec55a90',
-  verifications: '69441fd3d9350cee4e1b8e3f',
-  audit_logs: '694440eb0dc34459d50511ce'
+  modules: '69441e0e081da2e01f4d9a78',
+  workers: '69441f0deb5683351ec55a8f',
+  verifications: '69441fd3d9350cee4e1b8e3e',
+  audit_logs: '694440eb0dc34459d50511cd'  // also needs updating - middle segment
 }
 
 async function request(endpoint: string, options: RequestInit = {}) {
