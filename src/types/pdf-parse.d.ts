15343eaf70753174157fedd6c5334e817a6e6a2b
Agricog <mick@mickstevenson.com>
2025-12-22 12:53:39 +0000

    Create pdf-parse.d.ts

---
+++ src/types/pdf-parse.d.ts
@@ -0,0 +1,10 @@
+declare module 'pdf-parse' {
+  interface PDFData {
+    text: string
+    numpages: number
+    info: Record<string, unknown>
+  }
+  
+  function pdf(buffer: Buffer): Promise<PDFData>
+  export default pdf
+}
